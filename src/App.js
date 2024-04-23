a1592687bd3925db36083ed366c708ed623fd878
oussama007 <ohamdi776@gmail.com>
2024-04-23 18:31:56 +0100

    Update app component & styles

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import "./App.css";
-import Intro from "./components/Intro/Intro";
+import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
-import Services from "./components/Services/Services";
-
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Intro />
-      <Services />
+      <Home />
     </div>
   );
 }
 
 export default App;
-
-// Go Minute 00:05:00
-// Go Minute 00:20:30
-// Go Minute 00:30:00
-// Go Minute 00:50:00
-// Go Minute 00:59:00
