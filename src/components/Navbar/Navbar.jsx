b2c90871935df4d067c5fd2fa8b4a9c0e374616f
oussama007 <ohamdi776@gmail.com>
2024-04-23 18:33:43 +0100

    Create navbar component & styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,94 @@
-import React from "react";
+import { useEffect, useRef } from "react";
 import "./Navbar.css";
+import Icon from "@mdi/react";
+import { mdiWeatherNight } from "@mdi/js";
 
 const Navbar = () => {
+  const burgerMenuRef = useRef(null);
+  const linksParentRef = useRef(null);
+
+  useEffect(() => {
+    linksParentRef.current.onclick = (e) => {
+      e.stopPropagation();
+    };
+    burgerMenuRef.current.onclick = (e) => {
+      e.stopPropagation();
+      if (e.target.tagName === "SPAN") {
+        e.target.parentElement.classList.toggle("open");
+      }
+      linksParentRef.current.classList.toggle("active");
+      e.target.classList.toggle("open");
+    };
+    document.addEventListener("click", (e) => {
+      if (
+        e.target !== burgerMenuRef.current &&
+        e.target !== linksParentRef.current
+      ) {
+        if (linksParentRef.current.classList.contains("active")) {
+          linksParentRef.current.classList.toggle("active");
+          burgerMenuRef.current.classList.toggle("open");
+        }
+      }
+    });
+  }, []);
   return (
-    <div className="n-wrapper">
-      <div className="n-left">
-        <div className="n-name">Osama</div>
-        <span>Toggle</span>
-      </div>
-      <div className="n-right">
-        <div className="n-list">
-          <ul style={{ listStyleType: "none" }}>
-            <li>Home</li>
-            <li>Services</li>
-            <li>Experience</li>
-            <li>Portfolio</li>
-            <li>Testimonials</li>
-          </ul>
+    <nav className="navbar">
+      <div className="container">
+        <div className="nav-parent">
+          <div className="logo-parent">
+            <h1>
+              <a href="#home">
+                <svg viewBox="0 0 960 300">
+                  <symbol id="s-text">
+                    <text textAnchor="middle" x="50%" y="80%">
+                      Oussama
+                    </text>
+                  </symbol>
+                  <g className="g-ants">
+                    <use xlinkHref="#s-text" className="text-copy"></use>
+                    <use xlinkHref="#s-text" className="text-copy"></use>
+                    <use xlinkHref="#s-text" className="text-copy"></use>
+                    <use xlinkHref="#s-text" className="text-copy"></use>
+                    <use xlinkHref="#s-text" className="text-copy"></use>
+                  </g>
+                </svg>
+              </a>
+            </h1>
+          </div>
+          <div ref={burgerMenuRef} className="burger-menu">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div ref={linksParentRef} className="nav-links-parent">
+            <ul className="nav-links">
+              <li>
+                <a href="#home">Home</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#portfolio">Portfolio</a>
+              </li>
+              <li>
+                <a href="#skills">Skills</a>
+              </li>
+              <li>
+                <a href="#certifications">Certifications</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+              <li>
+                <Icon path={mdiWeatherNight} size={1} />
+              </li>
+            </ul>
+          </div>
         </div>
-        <button className="button n-button">Contact</button>
       </div>
-    </div>
+    </nav>
   );
 };
 
