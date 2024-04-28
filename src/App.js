502c3f8fadcc0fe79186da173a2d13ad68ff99a2
oussama007 <ohamdi776@gmail.com>
2024-04-28 15:21:13 +0100

    Add projects component to app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Projects />
     </div>
   );
 }
