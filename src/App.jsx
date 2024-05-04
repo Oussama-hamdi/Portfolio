e9e1d0ed99ff70d9c1db6102a5bd134936f1a45d
oussama007 <ohamdi776@gmail.com>
2024-05-04 22:36:20 +0100

    Add certifications and contact components to app component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar/Navbar";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
+import Certifications from "./components/Certifications/Certifications";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <About />
       <Projects />
       <Skills />
+      <Certifications />
+      <Contact />
     </main>
   );
 }
