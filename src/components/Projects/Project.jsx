66ad52c92faaed0f7e249c88cdb2407619b55df0
oussama007 <ohamdi776@gmail.com>
2024-05-08 20:39:17 +0100

    Add light theme styles to all the components

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -9,7 +9,7 @@ const defaultOptions = {
   reverse: false, // reverse the tilt direction
   max: 35,
   perspective: 800,
-  scale: 1, // 2 = 200%, 1.5 = 150%, etc..
+  scale: 1, // 2 = 200%, 1.5 = 150%
   speed: 1000, // Speed of the enter/exit transition
   transition: true, // Set a transition on enter/exit.
   axis: null, // What axis should be disabled. Can be X or Y.
